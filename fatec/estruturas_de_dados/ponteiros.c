58d8a1e760a26bfde704ee7457a9684dd2a53a67
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2020-08-28 17:37:54 -0300

    renomeando pasta, adicionando pasta para estrutura de dados, com um arquivo que lida com ponteiros.

---
+++ fatec/estruturas_de_dados/ponteiros.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int var;
+    int *ponteiro; //definindo ponteiro
+    var = 10;
+    ponteiro = &var; //agora o ponteiro aponta para var
+    printf("Conteudo de var: %d\n", var);
+    printf("Endereco de var: %p\n", &var);
+    printf("Conteudo de ponteiro: %p\n", ponteiro); //printa o endereço onde PONTEIRO está apontando. Ou seja, o mesmo endereço de VAR.
+    
+}
